893fd744932f1f76d6b23d7e74bd08ea73df1500
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-06-08 13:36:19 +0200

    unimportant fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User
 
   def self.find_or_create_from_auth_hash(auth_hash)
   	create! do |user|
-	    user.provider = auth_hash["provider"]
-	    user.uid = auth_hash["uid"]
-	    user.name = auth_hash["info"]["name"]
-	end
+        user.provider = auth_hash["provider"]
+        user.uid = auth_hash["uid"]
+        user.name = auth_hash["info"]["name"]
+    end
   end
 end
