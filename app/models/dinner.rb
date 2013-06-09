cdb3e75a3d38b54a3506110cd2210ef5b88c2435
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-06-09 13:28:05 +0200

    models validations

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -1,4 +1,7 @@
 class Dinner
   include Mongoid::Document
   field :name, type: String
+
+  validates :name, length: { in: 3..100 }, uniqueness: true
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,16 @@ class Order
   include Mongoid::Document
   field :status, type: Symbol, default: :waiting
   has_and_belongs_to_many :dinners, inverse_of: nil
-
-  validates :status, inclusion: {in: [:waiting, :not_sent, :sent]}
-
   belongs_to :user
+
+  validates :status, inclusion: { in: [:waiting, :not_sent, :sent] }
+  validates :user, presence: true
+
+  validate :validate_at_least_one_dinner
+
+  protected
+
+  def validate_at_least_one_dinner
+    errors.add(:dinners, "Order must have at least one dinner") if dinners.empty?
+  end
 end
