c0dd00c2240cce2dca620d9edea0b7dda4761cd7
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-06-09 14:44:59 +0200

    "najpopularniejsze obiady"

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -3,4 +3,18 @@ class Dinner
   field :name, type: String
 
   validates :name, length: { in: 3..100 }, uniqueness: true
+
+  def to_s
+    name
+  end
+
+  def self.get_top(limit = 10)
+    Order.all.flat_map(&:dinners)
+      .group_by(&:id)
+      .map{|k,list| [list.count, list.first]}
+      .sort_by(&:first)
+      .reverse
+      .map(&:last)
+      .take(limit)
+  end
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe OrdersController do
   context '#create' do
     it 'redirects to orders_path if save successful' do
-      subject.stub(:params).and_return(:form_action => 'Finish')
+      subject.stub(:params).and_return(form_action: 'Finish')
       subject.order.stub(:save).and_return(true)
       subject.should_receive(:redirect_to).with(orders_path)
     end
 
     it 'results in error if adding empty dinner' do
-      subject.stub(:params).and_return(:form_action => 'Finish')
+      subject.stub(:params).and_return(form_action: 'Finish')
       subject.order.stub(:save).and_return(false)
       subject.should_receive(:render).with(:new)
     end
 
     it 'adding a dinner continues the order' do
-      subject.stub(:params).and_return(:form_action => 'Add')
+      subject.stub(:params).and_return(form_action: 'Add')
       subject.should_receive(:render).with(:new)
     end
 
     before(:each) do
-      subject.stub(:current_user).and_return(nil)
+      subject.stub(current_user: nil)
     end
 
     after(:each) do
